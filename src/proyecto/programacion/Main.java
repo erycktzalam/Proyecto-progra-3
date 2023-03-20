291a4eadb1e02ca9103c5aaf7b550692cbaacca4
DaniSaurio <109646287+DaniSaurioM@users.noreply.github.com>
2023-03-19 19:45:47 -0600

    Menu
    Update, se agrega el menu.

--- src/proyecto/programacion/Main.java
+++ src/proyecto/programacion/Main.java
@@ -1,32 +1,73 @@
 package proyecto.programacion;
 
 import java.util.*;
+import javax.swing.JOptionPane;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese la expresion matematica:");
-        String expresion = sc.nextLine();
-        if (validarExpresion(expresion)) {
-            String expresionProcesada = procesar(expresion);
-            // Creamos el árbol binario y lo construimos a partir de la expresión
-            ArbolBinario arbol = new ArbolBinario();
-            String polaca = convertirPolaca(expresionProcesada);// convierte la expresion introducida polaca o preorden
-            arbol.crearArbol(polaca);// se crea el arbol a partir de la expresion
-            System.out.println("-----------Arbol Grafico-----------");
-            arbol.imprimirArbolVertical();
-            System.out.println("------------------------------------");
-            arbol.imprimirNotacionPolaca();
-            arbol.imprimirInorder();
-            arbol.imprimirNotacionpostorden();
-            // Recursividad
-            double resultado = resolverExpresion(arbol.raiz);
-            System.out.println("El resultado es: " + resultado);
-            System.out.println("hola mundo");
-        } else {
-            System.out.println("La expresion no es valida");
-        }
+        boolean salir = false, salirMenu = false;
+
+        do {
+
+            String expresion = JOptionPane.showInputDialog("Ingrese la expresion matematica");
+            if (validarExpresion(expresion)) {
+                String expresionProcesada = procesar(expresion);
+                // Creamos el árbol binario y lo construimos a partir de la expresión
+                ArbolBinario arbol = new ArbolBinario();
+                String polaca = convertirPolaca(expresionProcesada);// convierte la expresion introducida polaca o preorden
+                arbol.crearArbol(polaca);// se crea el arbol a partir de la expresion
+                System.out.println("-----------Arbol Grafico-----------");
+                arbol.imprimirArbolVertical();
+                System.out.println("------------------------------------");
+                do {
+                    // menu
+                    System.out.println("MENU");
+                    System.out.println("1. Imprimir notación polaca o prefija");
+                    System.out.println("2. Imprimir notacion inorder");
+                    System.out.println("3. Imprimir notación postfija");
+                    int opcion = sc.nextInt();
+                    sc.nextLine(); // Consumir la línea vacía después de leer la opción
+                    switch (opcion) {
+                        case 1:
+                            arbol.imprimirNotacionPolaca();
+                            break;
+                        case 2:
+                            arbol.imprimirInorder();
+                            break;
+                        case 3:
+                            arbol.imprimirNotacionpostorden();
+                            break;
+                        default:
+                            System.out.println("Opción inválida");
+                            break;
+                    }
+                    System.out.println("Desea imprimir alguna otra notacion?");
+                    System.out.println("1. si");
+                    System.out.println("2. no");
+                    int opcionMenu=sc.nextInt();
+                    sc.nextLine();
+                    if (opcionMenu!=1) {
+                        salirMenu = true;
+                    }
+                } while (!salirMenu);
+                // Recursividad
+                double resultado = resolverExpresion(arbol.raiz);
+                System.out.println("El resultado es: " + resultado);
+            } else {
+                System.out.println("La expresión no es válida");
+
+            }
+            System.out.println("Desea ingresar otra expresion?");
+            System.out.println("1. si");
+            System.out.println("2. no");
+            int opcionMenu=sc.nextInt();
+            sc.nextLine();
+            if (opcionMenu!=1) {
+                salir = true;
+            }
+        } while (!salir);
     }
 
     //Método recursivo que resuelve la expresión a partir de un nodo dado
